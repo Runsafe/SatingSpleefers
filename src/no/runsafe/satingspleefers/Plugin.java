91dd57b24215183edb160d0eb1840f0c76470d55
Morten Nilsen <morten@runsafe.no>
2013-12-20 13:07:01 +0100

    Update with new plugin template content

--- src/no/runsafe/satingspleefers/Plugin.java
+++ src/no/runsafe/satingspleefers/Plugin.java
@@ -1,16 +1,26 @@
 package no.runsafe.satingspleefers;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.api.log.IDebug;
 
 public class Plugin extends RunsafePlugin
+//public class Plugin extends RunsafeConfigurablePlugin
 {
-	public static IDebug Debugger = null;
+//	public static IDebug Debugger = null;
 
 	@Override
-	protected void PluginSetup()
+	protected void pluginSetup()
 	{
-		Debugger = getComponent(IDebug.class);
-		//addComponent(SomeComponent.class);
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(UniverseRegistration.class);
+//		addComponent(LUAScripts.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
 	}
 }
